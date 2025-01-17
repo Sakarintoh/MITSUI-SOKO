8fce6355ab25bd70cf8cdaeea81d0ef3c9d31759
Your Name <your-email@example.com>
2025-01-17 15:38:35 +0700

    MIT6

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from './Logo White MST.png';
 import './App.css';
 
 function App() {
-  const serverURL = 'http://localhost:5000'; // URL เซิร์ฟเวอร์
+  // กำหนด URL ของเซิร์ฟเวอร์โดยตรงในโค้ด
+  const serverURL = 'http://192.168.105.119:5000'; // เปลี่ยนเป็น URL เซิร์ฟเวอร์ของคุณ
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
   const [information, setInformation] = useState('');
@@ -101,19 +102,10 @@ function App() {
         <section id="section-2" className="section">
           <h1>Announcement</h1>
           <div className="buttons-section2">
-          <button className="btn-section2" onClick={() => handleShowAnnouncement('all')}>
-  <img src="house.png" alt="All" className="btn-logo" /> 
-</button>
-<button className="btn-section2" onClick={() => handleShowAnnouncement('IT')}>
-  <img src="it.png" alt="IT" className="btn-logo" /> 
-</button>
-<button className="btn-section2" onClick={() => handleShowAnnouncement('GA')}>
-  <img src="GA.png" alt="GA" className="btn-logo" /> 
-</button>
-<button className="btn-section2" onClick={() => handleShowAnnouncement('HR')}>
-  <img src="hr-manager.png" alt="HR" className="btn-logo" /> 
-</button>
-
+            <button className="btn-section2" onClick={() => handleShowAnnouncement('all')} />
+            <button className="btn-section2" onClick={() => handleShowAnnouncement('IT')} />
+            <button className="btn-section2" onClick={() => handleShowAnnouncement('GA')} />
+            <button className="btn-section2" onClick={() => handleShowAnnouncement('HR')} />
           </div>
           <div className="announcement-content">
             {selectedAnnouncement === 'all' && (
