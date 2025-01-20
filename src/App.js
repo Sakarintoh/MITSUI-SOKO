91be9f728131c9f1b7f3a5e59016aea03d77c9cb
Your Name <your-email@example.com>
2025-01-20 14:23:21 +0700

    MIT21

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
       .catch((error) => console.error('Error fetching host name:', error));
 
     // ดึงข้อความแชท
-    fetch('http://localhost:5000/messages')
+    fetch('http://192.168.7.94:5000/messages')
       .then((response) => response.json())
       .then((data) => setMessages(data))
       .catch((error) => console.error('Error fetching messages:', error));
